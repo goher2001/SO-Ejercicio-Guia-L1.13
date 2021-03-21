ecc2e900d91d041425fb84e4750172d00fe4e92e
christian gonzalez <christian.gonzalez@estudiantat.upc.edu>
2021-03-21 18:18:40 +0100

    Ejercicio guia L1.13

---
+++ ServidorrGrados/ServidorrGrados/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace ServidorrGrados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Celsius = new System.Windows.Forms.RadioButton();
+            this.Farenheit = new System.Windows.Forms.RadioButton();
+            this.numero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numero2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convertidor\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(499, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(499, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Celsius
+            // 
+            this.Celsius.AutoSize = true;
+            this.Celsius.Location = new System.Drawing.Point(245, 299);
+            this.Celsius.Name = "Celsius";
+            this.Celsius.Size = new System.Drawing.Size(112, 17);
+            this.Celsius.TabIndex = 3;
+            this.Celsius.TabStop = true;
+            this.Celsius.Text = "Convertir a Celsius";
+            this.Celsius.UseVisualStyleBackColor = true;
+            // 
+            // Farenheit
+            // 
+            this.Farenheit.AutoSize = true;
+            this.Farenheit.Location = new System.Drawing.Point(245, 324);
+            this.Farenheit.Name = "Farenheit";
+            this.Farenheit.Size = new System.Drawing.Size(123, 17);
+            this.Farenheit.TabIndex = 4;
+            this.Farenheit.TabStop = true;
+            this.Farenheit.Text = "Convertir a Farenheit";
+            this.Farenheit.UseVisualStyleBackColor = true;
+            // 
+            // numero
+            // 
+            this.numero.Location = new System.Drawing.Point(317, 182);
+            this.numero.Name = "numero";
+            this.numero.Size = new System.Drawing.Size(100, 20);
+            this.numero.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Inserte el número en Celsius\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(259, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 37);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Listo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(94, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Inserte el número en Farenheit\r\n";
+            // 
+            // numero2
+            // 
+            this.numero2.Location = new System.Drawing.Point(317, 237);
+            this.numero2.Name = "numero2";
+            this.numero2.Size = new System.Drawing.Size(100, 20);
+            this.numero2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numero2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numero);
+            this.Controls.Add(this.Farenheit);
+            this.Controls.Add(this.Celsius);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton Celsius;
+        private System.Windows.Forms.RadioButton Farenheit;
+        private System.Windows.Forms.TextBox numero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox numero2;
+    }
+}
+
---
+++ ServidorrGrados/ServidorrGrados/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+namespace ServidorrGrados
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9040);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            server.Connect(ipep);
+            this.BackColor = Color.Green;
+
+           
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Celsius.Checked)
+            {
+                if (numero2.Text == null)
+                {
+                    MessageBox.Show("No ha insertado un numero para convertir");
+                }
+                else
+                {
+                    string mensaje = "1/" + numero2.Text + "/";
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    MessageBox.Show("El resultado en Celsius es" + mensaje);
+                }
+            }
+            else
+            {
+                if (numero.Text == null)
+                {
+                    MessageBox.Show("No ha insertado un numero para convertir");
+                }
+                else
+                {
+                    string mensaje = "2/" + numero.Text + "/";
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                    server.Send(msg);
+
+                    byte[] msg2 = new byte[80];
+                    server.Receive(msg2);
+                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                    MessageBox.Show("El resultado en Farenheit es" + mensaje);
+                }
+               
+            }
+
+        }
+    }
+}
